959c07198431c569c60d0b26719a3c428316b047
Seow Chun Yong <seowcy@gmail.com>
2022-05-03 21:55:38 +0800

    Update and upload files
    Works, but without fuzzy matching of title

--- botty/botty_socket.py
+++ botty/botty_socket.py
@@ -1,16 +1,31 @@
 import os
+import json
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # Install the Slack app and get xoxb- token in advance
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
+results = {}
+# Load results
+with open("asp_results.json", 'rb') as f:
+    results = {**results, **json.load(f)}
+with open("csharp_results.json", 'rb') as f:
+    results = {**results, **json.load(f)}
+with open("java_results.json", 'rb') as f:
+    results = {**results, **json.load(f)}
+
+
 @app.event("message")
 def handle_message_events(body, say):
     print(body)
     user = body['event']['user']
     recv_text = body['event']['text']
-    say(channel=user, text="Received: %s" % recv_text)
+    if recv_text.lower() in results.keys():
+        resp = results[recv_text.lower()]
+        say(channel=user, text="```Title: %s\n```\n```Description:\n%s\n```\n```Impact:\n%s\n```\n```Mitigation:\n%s```\n" % (resp['title'], resp['desc'], resp['impact'], resp['mitigation']))
+    else:
+        say(channel=user, text="Received: %s" % recv_text)
     # logger.info(body)
 
 if __name__ == "__main__":
